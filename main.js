3902f7d65a9a22ccd7a64e3acc3fedbb44be88d2
Scott White <scott@Scotts-MacBook-Pro-2.local>
2017-05-08 23:33:23 -0400

    mid ie fixes

--- main.js
+++ main.js
@@ -1,3 +1,67 @@
+
+/*! http://mths.be/fromcodepoint v0.1.0 by @mathias */
+if (!String.fromCodePoint) {
+  (function() {
+    var defineProperty = (function() {
+      // IE 8 only supports `Object.defineProperty` on DOM elements
+      try {
+        var object = {};
+        var $defineProperty = Object.defineProperty;
+        var result = $defineProperty(object, object, object) && $defineProperty;
+      } catch(error) {}
+      return result;
+    }());
+    var stringFromCharCode = String.fromCharCode;
+    var floor = Math.floor;
+    var fromCodePoint = function() {
+      var MAX_SIZE = 0x4000;
+      var codeUnits = [];
+      var highSurrogate;
+      var lowSurrogate;
+      var index = -1;
+      var length = arguments.length;
+      if (!length) {
+        return '';
+      }
+      var result = '';
+      while (++index < length) {
+        var codePoint = Number(arguments[index]);
+        if (
+          !isFinite(codePoint) ||       // `NaN`, `+Infinity`, or `-Infinity`
+          codePoint < 0 ||              // not a valid Unicode code point
+          codePoint > 0x10FFFF ||       // not a valid Unicode code point
+          floor(codePoint) != codePoint // not an integer
+        ) {
+          throw RangeError('Invalid code point: ' + codePoint);
+        }
+        if (codePoint <= 0xFFFF) { // BMP code point
+          codeUnits.push(codePoint);
+        } else { // Astral code point; split in surrogate halves
+          // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+          codePoint -= 0x10000;
+          highSurrogate = (codePoint >> 10) + 0xD800;
+          lowSurrogate = (codePoint % 0x400) + 0xDC00;
+          codeUnits.push(highSurrogate, lowSurrogate);
+        }
+        if (index + 1 == length || codeUnits.length > MAX_SIZE) {
+          result += stringFromCharCode.apply(null, codeUnits);
+          codeUnits.length = 0;
+        }
+      }
+      return result;
+    };
+    if (defineProperty) {
+      defineProperty(String, 'fromCodePoint', {
+        'value': fromCodePoint,
+        'configurable': true,
+        'writable': true
+      });
+    } else {
+      String.fromCodePoint = fromCodePoint;
+    }
+  }());
+}
+
 (function () {
   var ew, ed, cc, maxc, endpos, trackingbox, trackpt;
   var LIMIT = 140;
@@ -38,49 +102,83 @@
   }
 
   // //http://stackoverflow.com/questions/4877326/how-can-i-tell-if-a-string-contains-multibyte-characters-in-javascript
-  // function surrogatePairToCodePoint(charCode1, charCode2) {
-  //   return ((charCode1 & 0x3FF) << 10) + (charCode2 & 0x3FF) + 0x10000;
-  // }
-  // function XXcleanText(str){
+  function surrogatePairToCodePoint(charCode1, charCode2) {
+    return ((charCode1 & 0x3FF) << 10) + (charCode2 & 0x3FF) + 0x10000;
+  }
+  function cleanTextWhat(str){
 
-  //   // Read string in character by character and create an array of code points
-  //   var codePoints = [], i = 0, charCode;
+    // Read string in character by character and create an array of code points
+    var codePoints = [], i = 0, charCode;
     
-  //   while (i < str.length) {
-  //     charCode = str.charCodeAt(i);
-  //     if ((charCode & 0xF800) == 0xD800) {
-  //       codePoints.push(surrogatePairToCodePoint(charCode, str.charCodeAt(++i)));
-  //     } else {
-  //       codePoints.push(charCode);
-  //     }
-  //     ++i;
-  //   }
-  //   var cleaned = '';
-  //   console.log('codepoints ', codePoints);
-  //   codePoints.forEach(function(c){
-  //     if(c <= 65535){
-  //       cleaned += String.fromCodePoint(c);
-  //     }
-  //   });
-  //   return cleaned;
-  // }
-
-  function cleanText(text){
-    var cleaned = '';
-    //does not work in IE/Safari, need alt solutions
-    for(var v of text){
-      if(v.length == 1){
-        cleaned += v;
+    while (i < str.length) {
+      charCode = str.charCodeAt(i);
+      if ((charCode & 0xF800) == 0xD800) {
+        codePoints.push(surrogatePairToCodePoint(charCode, str.charCodeAt(++i)));
+      } else {
+        codePoints.push(charCode);
       }
+      ++i;
     }
-    cleaned = cleaned.replace(/\u00A0/g,' ');
-    checkText(cleaned);
+    var cleaned = '';
+    console.log('codepoints ', codePoints);
+    codePoints.forEach(function(c){
+      if(c <= 65535){
+        cleaned += String.fromCodePoint(c);
+      }
+    });
     return cleaned;
-
   }
 
+  function fixedCharCodeAt(str, idx) {
+  // ex. fixedCharCodeAt('\uD800\uDC00', 0); // 65536
+  // ex. fixedCharCodeAt('\uD800\uDC00', 1); // false
+  idx = idx || 0;
+  var code = str.charCodeAt(idx);
+  var hi, low;
+  
+  // High surrogate (could change last hex to 0xDB7F
+  // to treat high private surrogates 
+  // as single characters)
+  if (0xD800 <= code && code <= 0xDBFF) {
+    hi = code;
+    low = str.charCodeAt(idx + 1);
+    if (isNaN(low)) {
+      throw 'High surrogate not followed by ' +
+        'low surrogate in fixedCharCodeAt()';
+    }
+    return ((hi - 0xD800) * 0x400) +
+      (low - 0xDC00) + 0x10000;
+  }
+  if (0xDC00 <= code && code <= 0xDFFF) { // Low surrogate
+    // We return false to allow loops to skip
+    // this iteration since should have already handled
+    // high surrogate above in the previous iteration
+    return false;
+    // hi = str.charCodeAt(idx - 1);
+    // low = code;
+    // return ((hi - 0xD800) * 0x400) +
+    //   (low - 0xDC00) + 0x10000;
+  }
+  return code;
+}
+
+  // function cleanText(text){
+  //   var cleaned = '';
+  //   //does not work in IE/Safari, need alt solutions
+  //   var v;
+  //   for(v of text){
+  //     if(v.length == 1){
+  //       cleaned += v;
+  //     }
+  //   }
+  //   cleaned = cleaned.replace(/\u00A0/g,' ');
+  //   checkText(cleaned);
+  //   return cleaned;
+
+  // }
+
   function insertText(cp, text) {
-    var cleaned = cleanText(text);
+    var cleaned = cleanTextWhat(text);
     if (cp == ed.textContent.length) {
       ed.textContent = ed.textContent + cleaned + ' ';
       return;
@@ -248,7 +346,7 @@
     var redpill = ed.getElementsByTagName('font')[0];
     var sel = window.getSelection();
     var edlength = ed.textContent.length;
-
+    var cp, range;
     if (edlength < adjustForSpaces()) {
       resetTracpt();
       if (!redpill) {
@@ -259,12 +357,12 @@
         }
         return;
       }
-      var cp = getCaretPosition(ed);
+      cp = getCaretPosition(ed);
       if (pasted === true) {
         cp = edlength;
       }
       var rt = redpill.textContent;
-      var range = document.createRange();
+      range = document.createRange();
       range.collapse(true);
       sel.removeAllRanges();
       wipe();
@@ -272,7 +370,7 @@
       sel.addRange(range);
       return;
     }
-    var cp = getCaretPosition(ed);
+    cp = getCaretPosition(ed);
     if (cp === edlength) {
       if (!overflow) {
         overflow = true;
@@ -292,7 +390,7 @@
     var tnode = ed.firstChild;
     var normaltext = tnode.textContent;
     var pos = adjustForSpaces();
-    var range = document.createRange();
+    range = document.createRange();
     range.collapse(true);
     sel.removeAllRanges();
 
